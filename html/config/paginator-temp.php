fd1715c1d1320830bda6e5cde31881547846d959
SuganoRyo <ryo22.0413@ezweb.ne.jp>
2023-11-22 21:11:34 +0900

    ページネーションの実装

---
+++ html/config/paginator-temp.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'number' => '<li class="page-item"><a class="page-link" href="{{url}}">{{text}}</a></li>',
+    'current' => '<li class="page-item active"><a class="page-link" href="{{url}}">{{text}}</a></li>',
+    'first' => '<li class="page-item"><a class="page-link" href="{{url}}" aria-label="Previous">
+    <span aria-hidden="true">{{text}}</span><span class="sr-only">Previous</span></a></li>',
+    'last' => '<li class="page-item"><a class="page-link" href="{{url}}" aria-label="Next">
+    <span aria-hidden="true">{{text}}</span><span class="sr-only">Next</span></a></li>',
+    'nextActive' => '<li class="page-item"><a class="page-link" href="{{url}}" aria-label="Next">
+    <span aria-hidden="true">{{text}}</span><span class="sr-only">Next</span></a></li>',
+    'prevActive' => '<li class="page-item"><a class="page-link" href="{{url}}" aria-label="Next">
+    <span aria-hidden="true">{{text}}</span><span class="sr-only">Next</span></a></li>',
+    'nextDisabled' => '<li class="page-item"><a class="page-link" href="{{url}}" aria-label="Next">
+    <span aria-hidden="true">{{text}}</span><span class="sr-only">Next</span></a></li>',
+    'prevDisabled' => '<li class="page-item"><a class="page-link" href="{{url}}" aria-label="Next">
+    <span aria-hidden="true">{{text}}</span><span class="sr-only">Next</span></a></li>',
+];
