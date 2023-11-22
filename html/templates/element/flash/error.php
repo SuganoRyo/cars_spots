9eaa0f9ea08c5cba2cd45c250e12fa774b66c7f8
SuganoRyo <ryo22.0413@ezweb.ne.jp>
2023-11-22 21:09:49 +0900

    フラッシュメッセージのデザイン及び日本語翻訳の修正

--- html/templates/element/flash/error.php
+++ html/templates/element/flash/error.php
@@ -8,8 +8,13 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="alert alert-warning" role="alert" style="margin-bottom: 0">
+<div class="alert alert-warning" role="alert" style="margin-bottom: 0; display: none">
     <p class="text-warning">
       <?= h($message); ?>
     </p>
 </div>
+<script>
+  $('.alert.alert-warning').fadeIn("slow", function () {
+    $(this).delay(2000).fadeOut("slow");
+  });
+</script>
--- html/templates/element/flash/success.php
+++ html/templates/element/flash/success.php
@@ -8,8 +8,13 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="alert alert-success" role="alert" style="margin-bottom: 0">
+<div class="alert alert-success" role="alert" style="margin-bottom: 0; display: none;">
     <p class="text-success">
       <?= h($message); ?>
     </p>
 </div>
+<script>
+  $('.alert.alert-success').fadeIn("slow", function () {
+    $(this).delay(2000).fadeOut("slow");
+  });
+</script>
